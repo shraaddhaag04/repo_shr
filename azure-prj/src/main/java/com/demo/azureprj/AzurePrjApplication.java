c44640800e446abd45a2649f0caaa9916344654f
AnkitAgrawal <agrawal.ank83@gmail.com>
2019-11-30 03:32:42 +0530

    Initial Development

---
+++ azure-prj/src/main/java/com/demo/azureprj/AzurePrjApplication.java
@@ -0,0 +1,13 @@
+package com.demo.azureprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzurePrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzurePrjApplication.class, args);
+	}
+
+}
---
+++ azure-prj/src/test/java/com/demo/azureprj/AzurePrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.azureprj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzurePrjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
